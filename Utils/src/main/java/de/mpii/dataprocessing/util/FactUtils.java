847f95a7564fb8992bbe49de4f8450419ef8932c
Mohamed Gad-Elrab <gadelrab@mpi-inf.mpg.de>
2018-05-10 20:56:46 +0200

    csv representation

--- Utils/src/main/java/de/mpii/dataprocessing/util/FactUtils.java
+++ Utils/src/main/java/de/mpii/dataprocessing/util/FactUtils.java
@@ -12,6 +12,19 @@ public class FactUtils {
         return predicateName;
     }
 
+    public static String getReadablePredicateName(String predicateName) {
+        if(predicateName.startsWith("<"))
+            predicateName= predicateName.replace("<","").replace(">","");
+
+
+        predicateName=predicateName.replaceAll(":","_").replaceAll("\\.","_");
+
+
+        predicateName=predicateName.replace(":"," ").replaceAll("^([a-z]{2,3})/","").replaceAll("(_\\(.+?\\))$"," ").trim().replace("_"," ").replaceAll("(?=\\p{Upper})"," ").replaceAll(" +", " ");
+
+        return predicateName;
+    }
+
     public static String getCleanName(String key) {
         if(key.startsWith("<"))
             key=key.replace("<","");
